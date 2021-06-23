9ea7b1e7d25e5837d2d3b2ea43113f135fc89d4c
Bongani Mbigi <bongani.mbigi@icloud.com>
2021-06-22 19:31:38 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.json({msg: `Hi from ${req.hostname}`})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
